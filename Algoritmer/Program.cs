7a5f564c9c717de8b93a20e4d308baf0be044598
Max Pohl <01max.pohl@gmail.com>
2023-09-06 20:01:37 +0200

    Added a few more algorithms to Algoritmer projekt

--- Algoritmer/Program.cs
+++ Algoritmer/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
 
-            while (true) 
+            while (true) //Kör förevigt
             {
                 Menu();
             }
@@ -71,12 +71,84 @@
 
         public static void AutoHome()
         {
+            Console.Clear();
+
+            string[] nummer = {"Ett", "Två", "Tre", "Fyra", "Fem", "Sex", "Sju", "Åtta", "Nio", "Tio", "Elva", "Tolv"}; //Nummer i text format i en lista
+            Console.Write("Tid på dygnet per heltimma (1-24): ");
+            int tid = Convert.ToInt32(Console.ReadLine());
+            string tidPåDagen = "ERROR";
+
+            if (tid > 12 && tid < 25)  //Konverterar 24 -> 12, 22 -> 10 osv... om numret är över 12
+                tidPåDagen = nummer[(tid - 12) - 1];
+            else if(tid < 12 && tid > 0)
+                tidPåDagen = nummer[tid - 1];
+
+
+
+
+            switch (tid > 24 || tid < 1 ? "Ogiltligt" : // Högre än 24 eller mindre än 1 (Ogiltligt)
+                    tid > 20 || tid < 7 ? "Natt" : // Högre än 20 eller mindre än 7 (21-06)
+                    tid > 17 && tid < 21 ? "Kväll" : // Högre än 17 och mindre än 21 (18-20)
+                    tid > 9 && tid < 18 ? "Dag" : "Morgon") // Högre än 9 och mindre än 18 (10-17) resten morgon (07-09)
+            {
+                case "Natt":
+                    Console.WriteLine($"{tidPåDagen} på natten: Alla sover, dörrar låsta, lampor av, gräsklippare av osv");
+                    break;
+                case "Morgon":
+                    Console.WriteLine($"{tidPåDagen} på morgonen: Kaffemaskinen på, lampor tända, lugn musik på");
+                    break;
+                case "Dag":
+                    Console.WriteLine($"{tidPåDagen} på dagen: Kaffemaskinen av, alla lampor av, musik av, dörrar låsta, diskmaskin på, tvättmaskin på");
+                    break;
+                case "Kväll":
+                    Console.WriteLine($"{tidPåDagen} på kvällen: Lampor på, musik av, TV på, gräsklippare på");
+                    break;
+                default:
+                    Console.WriteLine("Ogiltligt tal");
+                    break;
+            }
 
             Console.WriteLine($"\nKlicka någon knapp när du är klar...");
             Console.ReadKey();
         }
-        public static void TempConversion()
+        public static void TempConversion() //Omvandlar temperatur
         {
+            Console.Clear();
+
+            Console.Write("Ange Temperatur.\n" +
+                              "Temperaturen avslutas med\n" +
+                              "C för Celcius\n" +
+                              "F för Fahrenheit\n" +
+                              "K för Kelvin\n" +
+                              "Ange Temperatur: ");
+
+            string input = Console.ReadLine();
+            string tempStr = input.Remove(input.Length - 1, 1);
+            double temp = Convert.ToDouble(tempStr);
+            string type;
+
+            if(input.EndsWith('C'))
+            {
+                type = "Celcius";
+                Console.WriteLine($"{type} till Farenheit: {(temp * 1.8) + 32}\n" +
+                                  $"{type} till Kelvin: {temp + 273.15}\n");
+            }
+            else if(input.EndsWith('F'))
+            {
+                type = "Farenheit";
+                Console.WriteLine($"{type} till Celcius: {(temp - 32) / 1.8}\n" +
+                                  $"{type} till Kelvin: {(temp - 32) / 1.8 + 273.15}\n");
+            }
+            else if(input.EndsWith('K'))
+            {
+                type = "Kelvin";
+                Console.WriteLine($"{type} till Farenheit: {(temp -273.15) * 1.8 + 32}\n" +
+                                  $"{type} till Celcius: {temp - 273.15}\n");
+            }
+            else
+            {
+                Console.WriteLine("Invalid number");
+            }
 
             Console.WriteLine($"\nKlicka någon knapp när du är klar...");
             Console.ReadKey();
@@ -90,12 +162,13 @@
                               "1. Miniräknaren\n" +
                               "2. Magic numbers\n" +
                               "3. Automatic home\n" +
-                              "4. Tempraturomvandling");
+                              "4. Temperaturomvandling\n" +
+                              "----------------------------------------------");
 
-            var input = Console.ReadKey();
+            var input = Console.ReadKey(); //Registrerar vad du klickar på
             switch (input.Key)
             {
-                case ConsoleKey.D1:
+                case ConsoleKey.D1: //Om du har klickat på siffran 1...
                     Calc();
                     break;
                 case ConsoleKey.D2:
